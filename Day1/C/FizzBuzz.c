bc88049a1f1fa9d251fd57fbbca434e2cd16acb4
Vinay-Tare <44465147+Vinay-Tare@users.noreply.github.com>
2019-09-20 20:04:39 +0530

    Create FizzBuzz.c

---
+++ Day1/C/FizzBuzz.c
@@ -0,0 +1,28 @@
+/**
+        * @author: Vinay-Tare
+        * @date: 20/09/2019
+**/
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int i,no;
+clrscr();
+printf("Enter The No. Of Terms To Be Printed");
+scanf("%d",&i)	;
+ for(no=1;no<i;no++)
+ {
+ if(no%15==0)
+ printf("FizzBuzz ");
+ else if(no%5==0)
+ printf("Buzz ");
+ else if(no%3==0)
+ printf("Fizz ");
+ else
+ printf("%d ",no);
+ }
+getch();
+}
+
