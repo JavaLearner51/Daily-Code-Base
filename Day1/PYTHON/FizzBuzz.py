8062f7b22f5b47b42a7007d43db3274d517ec0b3
Vinay-Tare <44465147+Vinay-Tare@users.noreply.github.com>
2019-09-20 20:51:17 +0530

    Create FizzBuzz.py

---
+++ Day1/PYTHON/FizzBuzz.py
@@ -0,0 +1,13 @@
+ #    @author: <GitHub Username>
+ #    @date: <date>
+
+i=int(input("Enter The No. Of Terms You Want"))
+for no in range(1,i+1):
+    if no%15==0:
+     print("FizzBuzz",end=' ')
+    elif no%5==0:
+     print("Buzz",end=' ')
+    elif no%3==0:
+     print("Fizz",end=' ')
+    else:
+     print(no,end=' ')
